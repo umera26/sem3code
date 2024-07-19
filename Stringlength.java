b9a7b994c189c64637ebdd9b163845c6946faff4
umera26 <umera.235486201@vcet.edu.in>
2024-07-19 10:14:53 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,12 @@
+class StringLength
+{
+	public static void main(String args[])
+
+	{
+		int len=args.length;
+		for(int i=0;i<len;i++)
+		{
+	  		System.out.println("Args=" +args[i]);
+		}
+   	}
+}
